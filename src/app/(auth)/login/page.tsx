02686109d34228c5487cb4982b1ef63cf30c7606
Alan <aw4630@nyu.edu>
2024-10-27 02:04:10 -0400

    math page.jsx edited

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -12,6 +12,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+
 export default function LoginForm() {
   return (
     <Card className="mx-auto max-w-sm">
