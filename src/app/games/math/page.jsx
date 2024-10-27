28889538fe1e9bb1e7033e84447936489c2ca6ab
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-10-27 10:58:13 -0400

    matching game

--- src/app/games/math/page.jsx
+++ src/app/games/math/page.jsx
@@ -172,7 +172,7 @@ const MathGame = () => {
   return (
     <div className="container p-4 w-full flex flex-col">
       <div className="flex justify-left w-full">
-        <a href="/">
+        <a href="/dashboard">
           <svg viewBox="0 -960 960 960" width="24px" fill="#c8cacd">
             <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
           </svg>
