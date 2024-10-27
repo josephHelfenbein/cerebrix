85e306407bf9adf872de2258de1fd726f0974749
Alan <aw4630@nyu.edu>
2024-10-27 00:51:11 -0400

    chore: literal

--- src/app/voice/page.tsx
+++ src/app/voice/page.tsx
@@ -1,21 +1,22 @@
-import Client from '@/src/components/client';
-import {fetchAccessToken} from 'hume';
+import Client from '@/components/client';
+import { fetchAccessToken } from 'hume';
 
-export default async function Voice(){
+export default async function Voice() {
     const HUME_API_KEY = String(process.env.HUME_API_KEY);
     const HUME_SECRET_KEY = String(process.env.HUME_SECRET_KEY);
     const accessToken = await fetchAccessToken({
         apiKey: HUME_API_KEY,
         secretKey: HUME_SECRET_KEY,
-      });
-      if(!accessToken) throw Error();
+    });
+    if (!accessToken) { throw Error('No access token') };
     return (
-        <div className='flex justify-center h-screen'>
-            <div className='flex flex-col h-screen justify-between p-16'>
-                <h3 className='text-center text-2xl'>Personal Friend</h3>
-                <Client accessToken={accessToken}/>
+        <>
+            <div className='flex justify-center h-screen'>
+                <div className='flex flex-col h-screen justify-between p-16'>
+                    <h3 className='text-center text-2xl'>Personal Friend</h3>
+                    <Client accessToken={accessToken} />
+                </div>
             </div>
-        </div>
-        
+        </>
     )
 }
