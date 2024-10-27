3b8e3bd616b3857aa65b6c9406a53950263035eb
Alan <aw4630@nyu.edu>
2024-10-27 01:03:20 -0400

    commented out blog

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,9 @@
-import { getBlogPosts } from "@/lib/blog";
 import { MetadataRoute } from "next";
 import { headers } from "next/headers";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const allPosts = await getBlogPosts();
-  const headersList = headers();
+  // const allPosts = await getBlogPosts();
+  const headersList = await headers();
   let domain = headersList.get("host") as string;
   let protocol = "https";
 
@@ -13,9 +12,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${protocol}://${domain}`,
       lastModified: new Date(),
     },
-    ...allPosts.map((post) => ({
-      url: `${protocol}://${domain}/blog/${post.slug}`,
-      lastModified: new Date(),
-    })),
   ];
 }
